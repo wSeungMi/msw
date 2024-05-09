5608b9d565f490343c453b5c044b83e97050edc5
wSeungMi <seungmi.dev@gmail.com>
2024-05-09 21:11:04 +0900

    Feat: Home API 연결

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
+
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+
+  const setInitData = async () => {
+    const data = await fetchCountries();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
