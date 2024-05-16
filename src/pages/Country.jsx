8a0cf2cb66ce623afd84130dd418b96dc30b1677
wSeungMi <seungmi.dev@gmail.com>
2024-05-16 23:33:54 +0900

    Feat: Country 상세페이지 구현

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { fetchCountry } from "../api";
 import { useEffect, useState } from "react";
+import style from "./Country.module.css";
 
 export default function Country() {
   const params = useParams();
@@ -15,10 +16,41 @@ export default function Country() {
     setInitData();
   }, [params.code]);
 
+  if (!country) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <h1>Country Page</h1>
-      <div>Country: {params.code}</div>
-    </>
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.commonName}>
+          {country.flagEmoji}&nbsp;{country.commonName}
+        </div>
+        <div className={style.officialName}>{country.officialName}</div>
+      </div>
+      <img
+        src={country.flagImg}
+        alt={`${country.commonName}의 국기 이미지입니다.`}
+      />
+      <div className={style.body}>
+        <div>
+          <strong>코드: &nbsp;{country.code}</strong>
+        </div>
+        <div>
+          <strong>코드: &nbsp;{country.capital.join(", ")}</strong>
+        </div>
+        <div>
+          <strong>지역: &nbsp;{country.region}</strong>
+        </div>
+        <div>
+          <strong>
+            지도: &nbsp;
+            <a href={country.googleMapURL} target="_blank">
+              {country.googleMapURL}
+            </a>
+          </strong>
+        </div>
+      </div>
+    </div>
   );
 }
